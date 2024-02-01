4f5f6094276a358f6c1cb2052895e1b6da6dce41
jakir-ruet <jakir.ruet.bd@gmail.com>
2024-02-01 22:10:32 +0600

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>PHP Laravel application for ansible deployment</title>
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link rel="stylesheet" href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" />
+    </head>
+    <body>
+        <h1>PHP Laravel application for ansible deployment</h1>
+    </body>
+</html>
